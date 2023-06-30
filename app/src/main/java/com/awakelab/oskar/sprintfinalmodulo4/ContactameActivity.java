764a4bd51715cd155e3bedb79391007a734b391a
OskarUno <oorellana80@gmail.com>
2023-06-29 21:32:30 -0400

    Ejercicio Final de Modulo: Se agregan imagenes de background, link en linkedin, se bloquea la rotacion para la activity contactame

--- app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/ContactameActivity.java
+++ app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/ContactameActivity.java
@@ -2,6 +2,8 @@ package com.awakelab.oskar.sprintfinalmodulo4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +16,22 @@ public class ContactameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityContactameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initList();
+
+
+    }
+
+    private void initList() {
+        binding.btnLinkedin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent implicitIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.linkedin.com/posts/sideshowinc_race-toward-productivity-with-the-flash-activity-7061844791638044672-Ugje/?originalSubdomain=uy"));
+                startActivity(implicitIntent);
+            }
+        });
+
+
 
 
         setSupportActionBar(binding.toolbar);
