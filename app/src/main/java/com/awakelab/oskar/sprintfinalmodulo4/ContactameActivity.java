ecd8743695eab92be6e57d5c44506e5e45eba60f
OskarUno <oorellana80@gmail.com>
2023-06-29 22:12:48 -0400

    Ejercicio Final de Modulo: Se agrega intent para enviar mail

--- app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/ContactameActivity.java
+++ app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/ContactameActivity.java
@@ -1,11 +1,14 @@
 package com.awakelab.oskar.sprintfinalmodulo4;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.awakelab.oskar.sprintfinalmodulo4.databinding.ActivityContactameBinding;
 
@@ -30,6 +33,25 @@ public class ContactameActivity extends AppCompatActivity {
                 startActivity(implicitIntent);
             }
         });
+
+        String textomail = String.valueOf(binding.editTextMail.getText());
+        binding.btnEnviarMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent outlookEmail =  getPackageManager().getLaunchIntentForPackage("com.microsoft.office.outlook");
+                Intent gmailEmail = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
+                if (gmailEmail  != null || outlookEmail !=null) {
+                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","flash@gmail.com", null));
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto");
+                    emailIntent.putExtra(Intent.EXTRA_TEXT,textomail);
+                    startActivity(Intent.createChooser(emailIntent, "Enviar reporte: "));
+                }else{
+                    Toast.makeText(getBaseContext(), "No posees una aplicación para realizar esta acción.", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
 
 
 
