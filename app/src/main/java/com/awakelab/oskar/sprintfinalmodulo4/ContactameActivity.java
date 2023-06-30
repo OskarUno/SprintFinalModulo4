1fc9c1314db7c54eb0302f1f8d248c2c6822c0bf
OskarUno <oorellana80@gmail.com>
2023-06-30 01:10:17 -0400

    Ejercicio Final de Modulo: Se agrega intent para enviar whatsapp
    Se modificó el layout de la segunda actividad ya que se ocupó EditText para enviar mail o whatsapp, ademas tambien se le agregó un background para una mejor apariencia.

--- app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/ContactameActivity.java
+++ app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/ContactameActivity.java
@@ -1,19 +1,19 @@
 package com.awakelab.oskar.sprintfinalmodulo4;
 
-import static android.app.PendingIntent.getActivity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.awakelab.oskar.sprintfinalmodulo4.databinding.ActivityContactameBinding;
 
 public class ContactameActivity extends AppCompatActivity {
     private ActivityContactameBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,47 +21,45 @@ public class ContactameActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         initList();
 
-
     }
 
     private void initList() {
-        binding.btnLinkedin2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent implicitIntent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.linkedin.com/posts/sideshowinc_race-toward-productivity-with-the-flash-activity-7061844791638044672-Ugje/?originalSubdomain=uy"));
-                startActivity(implicitIntent);
+        binding.btnLinkedin2.setOnClickListener(v -> {
+            Intent implicitIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.linkedin.com/posts/sideshowinc_race-toward-productivity-with-the-flash-activity-7061844791638044672-Ugje/?originalSubdomain=uy"));
+            startActivity(implicitIntent);
+        });
+
+        binding.btnImgWzp.setOnClickListener(v -> {
+            PackageManager pm = getPackageManager();
+            try {
+                Intent waIntent = new Intent(Intent.ACTION_SEND);
+                waIntent.setType("text/plain");
+                PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+                waIntent.setPackage("com.whatsapp");
+                waIntent.putExtra(Intent.EXTRA_TEXT, String.valueOf(binding.editTextMail.getText()));
+                startActivity(Intent.createChooser(waIntent, "Compartir con:"));
+            } catch (PackageManager.NameNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "WhatsApp no esta instalado!", Toast.LENGTH_SHORT)
+                        .show();
             }
         });
 
-        String textomail = String.valueOf(binding.editTextMail.getText());
-        binding.btnEnviarMail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent outlookEmail =  getPackageManager().getLaunchIntentForPackage("com.microsoft.office.outlook");
-                Intent gmailEmail = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
-                if (gmailEmail  != null || outlookEmail !=null) {
-                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","flash@gmail.com", null));
-                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto");
-                    emailIntent.putExtra(Intent.EXTRA_TEXT,textomail);
-                    startActivity(Intent.createChooser(emailIntent, "Enviar reporte: "));
-                }else{
-                    Toast.makeText(getBaseContext(), "No posees una aplicación para realizar esta acción.", Toast.LENGTH_LONG).show();
-                }
-
+        binding.btnImgMail.setOnClickListener(v -> {
+            String textomails = String.valueOf(binding.editTextMail.getText());
+            if(textomails.trim().isEmpty()){
+            Toast.makeText(this,"No puede tener el texto vacio",Toast.LENGTH_SHORT).show();
+            }else {
+                Intent mailIntent = new Intent(Intent.ACTION_SEND);
+                mailIntent.setType("text/plain");
+                mailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"flash@gmail.com"});
+                mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Enviado desde App");//se usará por ejemplo para email
+                mailIntent.putExtra(Intent.EXTRA_TEXT, textomails);
+                startActivity(Intent.createChooser(mailIntent, "Creando correo...."));
             }
         });
-
-
-
-
 
         setSupportActionBar(binding.toolbar);
-        binding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        binding.toolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
 }
--- app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/MainActivity.java
+++ app/src/main/java/com/awakelab/oskar/sprintfinalmodulo4/MainActivity.java
@@ -1,14 +1,15 @@
 package com.awakelab.oskar.sprintfinalmodulo4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.awakelab.oskar.sprintfinalmodulo4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
